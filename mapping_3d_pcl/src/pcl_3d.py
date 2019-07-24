87b82948023b18a63880d3794c9822fcd2c279b9
Stratos <53256050+StratosIlios@users.noreply.github.com>
2019-07-24 15:41:47 +0200

    Update pcl_3d.py

--- mapping_3d_pcl/src/pcl_3d.py
+++ mapping_3d_pcl/src/pcl_3d.py
@@ -4,7 +4,6 @@ import rospy
 from laser_assembler.srv import AssembleScans
 from sensor_msgs.msg import PointCloud
 
-
 rospy.init_node("pcl_3d")
 #blocks until service is available
 rospy.wait_for_service("assemble_scans")
@@ -13,16 +12,15 @@ assemble_scans=rospy.ServiceProxy("assemble_scans",AssembleScans)
 #Publisher and topic, (topic name, message type, limits the amount of queued messages)
 point_cloud_publisher_=rospy.Publisher("/cloud", PointCloud, queue_size=1)
 
-
 #Creates a Rate object rate, 10Hz
 rate=rospy.Rate(10)
 
 while(True):
 	try:
-		#Call and response of the service. Time interval(Time instance, get current time)
-		response=assemble_scans(rospy.Time(0,0),rospy.get_rostime())
-		point_cloud_publisher_.publish(response.cloud)
+	       #Call and response of the service. Time interval(Time instance, get current time)
+	       response=assemble_scans(rospy.Time(0,0),rospy.get_rostime())
+	       point_cloud_publisher_.publish(response.cloud)
 	except rospy.ServiceException, e:
-		   print "Service is failed: %s" %e
+	       print "Service is failed: %s" %e
 
 	rate.sleep()
